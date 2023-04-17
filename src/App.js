8f7b49dd19e1cffb7613a3ef4221dccebbe64119
yichen <chen1241548625@gmail.com>
2023-04-17 19:53:25 -0400

    init react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <form>
+  <div>Enter Your Name</div>
+  <input type="text"></input>
+  <button>submit!</button>
+  </form>
+    </div>
+  );
+}
+
+export default App;
